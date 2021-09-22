555519edcfa79402a709f5c203fc91fa7e866b52
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2021-09-22 13:35:00 +0200

    Added VM update (#38)
    This change adds the ability to update the VM name and comment.

--- client_vm_test.go
+++ client_vm_test.go
@@ -22,7 +22,7 @@ func TestAfterVMCreationShouldBePresent(t *testing.T) {
 	vm, err := client.CreateVM(
 		helper.GetClusterID(),
 		helper.GetBlankTemplateID(),
-		ovirtclient.CreateVMParams().WithName("test"),
+		ovirtclient.CreateVMParams().MustWithName("test"),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -42,5 +42,35 @@ func TestAfterVMCreationShouldBePresent(t *testing.T) {
 	}
 	if fetchedVM.ID() != vm.ID() {
 		t.Fatalf("fetched VM ID %s mismatches original created VM ID %s", fetchedVM.ID(), vm.ID())
+	}
+
+	updatedVM, err := fetchedVM.Update(
+		ovirtclient.UpdateVMParams().MustWithName("new_name").MustWithComment("new comment"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if updatedVM.ID() != vm.ID() {
+		t.Fatalf("updated VM ID %s mismatches original created VM ID %s", updatedVM.ID(), vm.ID())
+	}
+	if updatedVM.Name() != "new_name" {
+		t.Fatalf("updated VM name %s does not match update parameters", updatedVM.Name())
+	}
+	if updatedVM.Comment() != "new comment" {
+		t.Fatalf("updated VM comment %s does not match update parameters", updatedVM.Comment())
+	}
+
+	fetchedVM, err = client.GetVM(vm.ID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if fetchedVM == nil {
+		t.Fatal("returned VM is nil")
+	}
+	if fetchedVM.Name() != "new_name" {
+		t.Fatalf("updated VM name %s does not match update parameters", fetchedVM.Name())
+	}
+	if fetchedVM.Comment() != "new comment" {
+		t.Fatalf("updated VM comment %s does not match update parameters", fetchedVM.Comment())
 	}
 }
