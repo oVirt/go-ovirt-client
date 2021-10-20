f50fbcb4b1f1c037bf9698ad2aab5c22fcb228eb
Janos <86970079+janosdebugs@users.noreply.github.com>
2021-10-20 14:23:11 +0200

    Bugfix: searching for a non-existent disk alias should not result in an error.

--- mock_disk_list_by_alias.go
+++ mock_disk_list_by_alias.go
@@ -9,8 +9,5 @@ func (m *mockClient) ListDisksByAlias(alias string, _ ...RetryStrategy) ([]Disk,
 			result = append(result, d)
 		}
 	}
-	if len(result) == 0 {
-		return nil, newError(ENotFound, "disk with name %s not found", alias)
-	}
 	return result, nil
 }
