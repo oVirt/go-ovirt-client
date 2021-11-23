7cfec595d3425752186cb03e4bc05d1848da57bd
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2021-11-23 10:59:11 +0100

    Added check to disallow removing disks from running VMs (#77)

--- mock_disk_remove.go
+++ mock_disk_remove.go
@@ -8,6 +8,14 @@ func (m *mockClient) RemoveDisk(diskID string, _ ...RetryStrategy) error {
 		return newError(ENotFound, "disk with ID %s not found", diskID)
 	}
 
+	// Check if disk is attached to a running VM
+	if diskAttachment, ok := m.diskAttachmentsByDisk[diskID]; ok {
+		vm := m.vms[diskAttachment.vmid]
+		if vm.status != VMStatusDown {
+			return newError(EConflict, "Disk %s is attached to VM %s", diskID, vm.id)
+		}
+	}
+
 	delete(m.diskAttachmentsByDisk, diskID)
 	delete(m.disks, diskID)
 
