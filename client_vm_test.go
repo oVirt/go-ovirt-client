c7adf41a545ff6d4c93b69f4f1b908b1ac4e1c20
Evgeny Slutsky <eslutsky@redhat.com>
2022-04-04 12:46:10 +0300

    add test TestVMCreationWithDefaultMemory to check default memory
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- client_vm_test.go
+++ client_vm_test.go
@@ -147,12 +147,30 @@ func TestVMCreationWithMemory(t *testing.T) {
 		t,
 		helper,
 		fmt.Sprintf("test-%s", helper.GenerateRandomID(5)),
-		ovirtclient.CreateVMParams().MustWithMemory(&testMemory),
+		ovirtclient.CreateVMParams().MustWithMemory(testMemory),
 	)
 
 	memory := vm.Memory()
-	if *memory != testMemory {
-		t.Fatalf("Creating a VM with Memory settings did not return a VM with expected Memory , %d , %d.", memory, &testMemory)
+	if memory != testMemory {
+		t.Fatalf("Creating a VM with Memory settings did not return a VM with expected Memory , %d , %d.", memory, testMemory)
+	}
+
+}
+
+func TestVMCreationWithDefaultMemory(t *testing.T) {
+	t.Parallel()
+	defaultMemory := int64(1073741824)
+	helper := getHelper(t)
+	vm := assertCanCreateVM(
+		t,
+		helper,
+		fmt.Sprintf("test-%s", helper.GenerateRandomID(5)),
+		ovirtclient.CreateVMParams(),
+	)
+
+	memory := vm.Memory()
+	if memory != defaultMemory {
+		t.Fatalf("Creating a VM with a Default Memory settings did not return a VM with expected Memory , %d , %d.", memory, defaultMemory)
 	}
 
 }
