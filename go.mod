11bf65ed7e40fa4cbf7796ae7b06fba9a3e8eb91
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-04-27 11:05:21 +0200

    Fixes #183: Adding support for proxies

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/ovirt/go-ovirt v0.0.0-20210809163552-d4276e35d3db
+	github.com/ovirt/go-ovirt v0.0.0-20220427092237-114c47f2835c
 	github.com/ovirt/go-ovirt-client-log/v2 v2.2.0
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- new_test.go
+++ new_test.go
@@ -1,10 +1,15 @@
 package ovirtclient_test
 
 import (
+	"crypto/tls"
 	"crypto/x509"
 	"errors"
 	"fmt"
+	"io"
+	"net"
+	"net/http"
 	"strings"
+	"sync"
 	"testing"
 
 	ovirtclient "github.com/ovirt/go-ovirt-client"
@@ -119,7 +124,7 @@ func TestBadTLS(t *testing.T) {
 		t.Fatalf("failed to create server certificate (%v)", err)
 	}
 
-	port := getNextFreePort()
+	port := getNextFreePort(t)
 
 	srv, err := newTestServer(port, serverCert, serverPrivKey, &noopHandler{})
 	if err != nil {
@@ -152,4 +157,150 @@ func TestBadTLS(t *testing.T) {
 	} else {
 		t.Fatalf("the returned error was not an EngineError (%v)", err)
 	}
+}
+
+func TestProxy(t *testing.T) {
+	counter := 0
+	proxy := startProxyServer(t, &counter)
+	t.Parallel()
+	// Real CA is the CA we will use in the server
+	realCAPrivKey, realCACert, realCABytes, err := createCA()
+	if err != nil {
+		t.Fatalf("failed to create real CA (%v)", err)
+	}
+
+	serverPrivKey, serverCert, err := createSignedCert(
+		[]x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		realCAPrivKey,
+		realCACert,
+	)
+	if err != nil {
+		t.Fatalf("failed to create server certificate (%v)", err)
+	}
+
+	port := getNextFreePort(t)
+
+	srv, err := newTestServer(port, serverCert, serverPrivKey, &unauthorizedHandler{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := srv.Start(); err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(srv.Stop)
+
+	logger := ovirtclientlog.NewTestLogger(t)
+	_, err = ovirtclient.New(
+		fmt.Sprintf("https://127.0.0.1:%d", port),
+		"admin@internal",
+		"asdf",
+		ovirtclient.TLS().CACertsFromMemory(realCABytes),
+		logger,
+		ovirtclient.NewExtraSettings().WithProxy(fmt.Sprintf("http://%s", proxy)),
+	)
+	if err == nil {
+		t.Fatalf("No error on establishing a connection (%v)", err)
+	}
+	if !ovirtclient.HasErrorCode(err, ovirtclient.EAccessDenied) {
+		t.Fatalf("incorrect error code returned: %v", err)
+	}
+	if counter == 0 {
+		t.Fatalf("Connection did not go through the proxy.")
+	}
+}
+
+func startProxyServer(t *testing.T, counter *int) string {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("failed to open listen socket for the proxy (%v)", err)
+	}
+	var serverError error
+	srv := http.Server{
+		Addr: ln.Addr().String(),
+		// Disable HTTP/2.
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	srv.Handler = &proxyHandler{counter: counter}
+	go func() {
+		defer wg.Done()
+		if err := srv.Serve(ln); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				serverError = err
+			}
+		}
+	}()
+	t.Cleanup(
+		func() {
+			if err := srv.Close(); err != nil {
+				t.Fatalf("failed to close listen socket (%v)", err)
+			}
+			wg.Wait()
+			if serverError != nil {
+				t.Fatalf("proxy server stopped unexpectedly (%v)", err)
+			}
+		})
+	return ln.Addr().String()
+}
+
+type proxyHandler struct {
+	counter *int
+}
+
+func (p *proxyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	*p.counter++
+	if request.Method != "CONNECT" {
+		writer.WriteHeader(400)
+		return
+	}
+	target := request.URL.Host
+
+	hijacker, ok := writer.(http.Hijacker)
+	if !ok {
+		writer.WriteHeader(500)
+		return
+	}
+
+	backendConn, err := net.Dial("tcp", target)
+	if err != nil {
+		writer.WriteHeader(500)
+		return
+	}
+
+	writer.WriteHeader(200)
+	conn, clientBuf, err := hijacker.Hijack()
+	if err != nil {
+		return
+	}
+	if clientBuf != nil {
+		bytes := clientBuf.Reader.Buffered()
+		buf := make([]byte, bytes)
+		n, err := clientBuf.Read(buf)
+		if err != nil {
+			return
+		}
+		if n < bytes {
+			// This is a bug
+			return
+		}
+		if len(buf) > 0 {
+			_, _ = backendConn.Write(buf)
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(backendConn, conn)
+		_ = backendConn.Close()
+	}()
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(conn, backendConn)
+		_ = conn.Close()
+	}()
+	wg.Wait()
 }
